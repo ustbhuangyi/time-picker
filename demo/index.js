9f355395d719f1f36739fa5db9776d2845137b29
ustbhuangyi <280309453@qq.com>
2016-03-11 20:28:00 +0800

    build demo

---
+++ demo/index.js
@@ -0,0 +1,21 @@
+FastClick.attach(document.body);
+
+var $date = $('#date');
+
+$date.timePicker({
+	delay: 15,
+	day: {
+		len: 7,
+		filter: ['今天', '明天', '后天'],
+		format: 'M月d日'
+	}
+}).on('timePicker.select', function (e, selectedTime, selectedText) {
+	$(this).text(selectedText);
+});
+
+$date.on('click', function () {
+	$(this).timePicker('show');
+});
+
+
+
